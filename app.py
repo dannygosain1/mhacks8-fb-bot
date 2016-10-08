924ad05d8893b36427b6ee02e936c955d1f2a95e
Sadruddin Hashmani <sadu99@hotmail.com>
2016-10-08 18:16:55 -0400

    testing spacing

--- app.py
+++ app.py
@@ -22,9 +22,7 @@ mongo = PyMongo(app)
 
 # Send help message to user
 def send_help_message(sender_id):
-    help_message = """ Usage: [option] ... [argument]:
-                       portfolio [buy | sell | show]: update exisiting protfolio 
-                       analysis [ | ]: analyze portfolio risk """
+    help_message = "Usage: [option] ... [argument]: portfolio [buy | sell | show]: update exisiting protfolio analysis [ | ]: analyze portfolio risk"
     send_message(sender_id, help_message)
 
 # Send greeting message to user
